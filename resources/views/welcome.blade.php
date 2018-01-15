a36fa5f0a43cb49eddbf2d376118c93b6d3d7556
Jonathan Courat <jonathan.courat@Digital-Grenoble.local>
2018-01-15 11:47:41 +0100

    change laravel to HELLO WORLD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HELLO WORLD
                 </div>
 
                 <div class="links">
